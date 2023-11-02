217f9bd3b89f614705d827e138839bfcd443ca6f
spideydamn <115349892+spideydamn@users.noreply.github.com>
2023-11-02 11:49:35 +0300

    fix checkstyle and tests

--- src/main/java/edu/hw2/Task3.java
+++ src/main/java/edu/hw2/Task3.java
@@ -1,14 +1,12 @@
 package edu.hw2;
 
 import java.util.Random;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
+@SuppressWarnings("RegexpSinglelineJava")
 public class Task3 {
     private Task3() {}
 
     static Random rnd = new Random();
-    private final static Logger LOGGER = LogManager.getLogger();
     final static int FAILURE_PROBABILITY = 4;
 
     static void setRandomSeed(int seed) { // for testing
@@ -53,7 +51,7 @@ public class Task3 {
                     return;
                 } catch (Exception e) {
                     this.cause = e;
-                    LOGGER.info(e.getMessage());
+                    System.out.println(e.getMessage());
                 }
             }
         }
@@ -62,12 +60,12 @@ public class Task3 {
     static class StableConnection implements Connection {
         @Override
         public void execute(String command) {
-            LOGGER.info(String.format("Stable connection established. Sent command \"%s\"", command));
+            System.out.printf("Stable connection established. Sent command \"%s\"%n", command);
         }
 
         @Override
         public void close() {
-            LOGGER.info("Stable connection closed");
+            System.out.println("Stable connection closed");
         }
     }
 
@@ -77,12 +75,12 @@ public class Task3 {
             if (rnd.nextInt() % FAILURE_PROBABILITY == 0) {
                 throw new ConnectionException("Faulty connection failed");
             }
-            LOGGER.info(String.format("Faulty connection established. Sent command \"%s\"", command));
+            System.out.printf("Faulty connection established. Sent command \"%s\"%n", command);
         }
 
         @Override
         public void close() {
-            LOGGER.info("Faulty connection closed");
+            System.out.println("Faulty connection closed");
         }
     }
 
